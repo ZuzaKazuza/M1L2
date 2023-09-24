2d312fb014bae950896a6ac9f54f179072eb1ef9
ZuzaKazuza <143947205+ZuzaKazuza@users.noreply.github.com>
2023-09-24 18:11:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-import random
+import discord
+from discord.ext import commands
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
+intents = discord.Intents.default()
+intents.message_content = True
 
-    for i in range(pass_length):
-        password += random.choice(elements)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-    return password
+from bot_logic import gen_pass
+from bot_logic import flip_coin
 
-def flip_coin():
-    flip = random.randint(0, 2)
-    if flip == 0:
-        return "Heads"
-    else:
-        return "Tails"
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(f'gen_pass(7)')
+
+@bot.command()
+async def FlipCoin(ctx):   
+    await ctx.send(f'FlipCoin')                    
+   
+
+bot.run("")
